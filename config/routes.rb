1399d9d6cf2c75fe1895a283e9c0d6606858fc4e
hinohara <hinohara@hinoharanoMacBook-Air.local>
2019-06-01 10:18:22 +0900

    modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   get '/signup', to: 'users#new'
   get '/about', to: "static_pages#about"
   root 'static_pages#home'
-  resources :users
+  resources :users, only:[:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
