4f642558d89f7c731e28b554a6aac17fe6cfa577
hinohara <hinohara@hinoharanoMacBook-Air.local>
2019-06-08 19:52:48 +0900

    add rubocop

--- spec/controller/static_pages_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe StaticPagesController, type: :controller do
-  describe '#home' do
-    it "responds successfully" do
-      expect(response).to be_successful
-    end
-  end
-end
